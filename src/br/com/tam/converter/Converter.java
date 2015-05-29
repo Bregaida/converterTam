864e82d604c5b7aaf3e459c47c82a4a573bec2fa
Eduardo Bregaida <eduardo.bregaida@gmail.com>
2015-05-29 01:23:35 -0300

    Já separa por vírgula e coloca entre aspas. Falta: Remover o cabeçalho, melhorar a quebra de linhas

--- src/br/com/tam/converter/Converter.java
+++ src/br/com/tam/converter/Converter.java
@@ -4,13 +4,11 @@
 package br.com.tam.converter;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Scanner;
 
 /**
  * @author eduardobregaida
@@ -18,37 +16,53 @@ import java.net.URL;
  */
 public class Converter {
 
-    public void getPage(URL url, File file) throws IOException {
-        BufferedReader in =
-                new BufferedReader(new InputStreamReader(url.openStream()));
+	public static void main(String[] args) {
+		URL url = null;
+		Scanner in = null;
+		FileWriter writer = null;
+		try {
+
+			url = new URL(
+					"http://www.cgna.gov.br/rpl/Companhias/Cia_TAM_CS.txt");
+			BufferedReader bf = new BufferedReader(new InputStreamReader(
+					url.openStream()));
+			File arquivoNovo = new File("arquivoNovo.csv");
+
+			writer = new FileWriter(arquivoNovo);
+
+			in = new Scanner(bf);
+			in.useDelimiter("\\s{1,}");
+			String cabecalho = "code,flightnum,depicao,arricao,route,aircraft,flightlevel,distance,deptime,arrtime,flighttime,notes,price,flighttype,daysofweek,enabled,";
+			writer.write(cabecalho + "\"" + " week1" + "\"" + "," + "\""
+					+ " week2" + "\"" + "," + "\"" + " week3" + "\"" + ","
+					+ "\"" + " week4");
+			writer.append("\n");
+			while (in.hasNext()) {
+				writer.write("\"");
+				writer.write(in.next());//replace para remover o cabeçalho original?
+				/*
+		                                  	  Planos de Vôo Repetitivos
+                                               Classificação: Ident Anv 
  
-         BufferedWriter out = new BufferedWriter(new FileWriter(file));
- 
-        String inputLine;
- 
-        while ((inputLine = in.readLine()) != null) {
-            // Imprime página no console
-            System.out.println(inputLine);
-            // Grava pagina no arquivo
-            out.write(inputLine);
-            out.newLine();
-        }
- 
-        in.close();
-        out.flush();
-        out.close();
-    }
- 
-    public static void main(String[] args) {
-        URL url = null;
-        File file = new File("myFile.csv");
-        try {
-            url = new URL("http://www.cgna.gov.br/rpl/Companhias/Cia_TAM_CS.txt");
-            new Converter().getPage(url, file);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+CIA: TAM                                    INÍCIO DE VALIDADE: 28mai2015                                       PAG.: 1
+-----------------------------------------------------------------------------------------------------------------------------------------
+   VALIDO VALIDO DIAS OP  IDENT  TIPO   ADEP     VEL   FL  ROTA                                DEST         OBSERVACOES
+   DESDE   ATE   STQQSSD   ANV     TURB     EOBT                                                   EET
+-----------------------------------------------------------------------------------------------------------------------------------------
+				 */
+				writer.write("\"");
+				writer.append(", ");
+				
+			}
+			writer.append("\n"); //Melhorar a quebra de linha do inicio do original sempre pula
+			in.close();
+			writer.flush();
+			writer.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			System.out.println("Operação Finalizada");
+
+		}
+	}
 }
