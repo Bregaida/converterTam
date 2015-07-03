f8a63fbfc99081bd91a54e840e5614b93a019b05
Eduardo Bregaida <eduardo.bregaida@gmail.com>
2015-07-02 21:53:28 -0300

    Efetuado o processamento de forma satisfatória para o problema, gerando o CSV esperado

--- src/br/com/tam/converter/Converter.java
+++ src/br/com/tam/converter/Converter.java
@@ -5,10 +5,14 @@ package br.com.tam.converter;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileWriter;
+import java.io.FileReader;
 import java.io.InputStreamReader;
+import java.io.LineNumberReader;
 import java.net.URL;
 import java.util.Scanner;
+import java.util.function.Predicate;
+
+import org.apache.commons.io.output.FileWriterWithEncoding;
 
 /**
  * @author eduardobregaida
@@ -19,16 +23,16 @@ public class Converter {
 	public static void main(String[] args) {
 		URL url = null;
 		Scanner in = null;
-		FileWriter writer = null;
+		FileWriterWithEncoding writer = null;
 		try {
 
 			url = new URL(
 					"http://www.cgna.gov.br/rpl/Companhias/Cia_TAM_CS.txt");
 			BufferedReader bf = new BufferedReader(new InputStreamReader(
-					url.openStream()));
+					url.openStream(), "UTF-8"));
 			File arquivoNovo = new File("arquivoNovo.csv");
 
-			writer = new FileWriter(arquivoNovo);
+			writer = new FileWriterWithEncoding(arquivoNovo, "UTF-8");
 
 			in = new Scanner(bf);
 			in.useDelimiter("\\s{1,}");
@@ -38,23 +42,14 @@ public class Converter {
 					+ "\"" + " week4");
 			writer.append("\n");
 			while (in.hasNext()) {
+
 				writer.write("\"");
-				writer.write(in.next());//replace para remover o cabeçalho original?
-				/*
-		                                  	  Planos de Vôo Repetitivos
-                                               Classificação: Ident Anv 
- 
-CIA: TAM                                    INÍCIO DE VALIDADE: 28mai2015                                       PAG.: 1
------------------------------------------------------------------------------------------------------------------------------------------
-   VALIDO VALIDO DIAS OP  IDENT  TIPO   ADEP     VEL   FL  ROTA                                DEST         OBSERVACOES
-   DESDE   ATE   STQQSSD   ANV     TURB     EOBT                                                   EET
------------------------------------------------------------------------------------------------------------------------------------------
-				 */
+				writer.write(in.next());
 				writer.write("\"");
 				writer.append(", ");
-				
 			}
-			writer.append("\n"); //Melhorar a quebra de linha do inicio do original sempre pula
+			writer.append("\n"); //Melhorar a quebra de linha do inicio do
+			// original sempre pula
 			in.close();
 			writer.flush();
 			writer.close();
@@ -65,4 +60,5 @@ CIA: TAM                                    INÍCIO DE VALIDADE: 28mai2015      
 
 		}
 	}
+
 }
